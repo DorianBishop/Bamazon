a83e08a071480a3f235b585e02e2016bc2d3effe
Dorian Bishop <dorianbishop427@gmail.com>
2019-11-18 13:18:53 -0500

    added code to js.file

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,4 +1,17 @@
 // Bamazon Setup
 var mysql = require('mysql');
 var inquirer = require('inquirer');
-var Table = require('cli-table');
+var Table = require('cli-table2');
+
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: '',
+    password: '',
+    database: 'bamazon_db'
+});
+
+connection.connect((err) => {   if (err) throw err;
+    console.log('Connection successful');
+
+    // Will display all items from  the database , when mysql has been established. 
