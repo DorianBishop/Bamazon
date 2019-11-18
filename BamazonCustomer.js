ba2b60e2761a01f6e18477933bab5cb55a30a692
Dorian Bishop <dorianbishop427@gmail.com>
2019-11-18 13:20:44 -0500

    items display added

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -15,3 +15,5 @@ connection.connect((err) => {   if (err) throw err;
     console.log('Connection successful');
 
     // Will display all items from  the database , when mysql has been established. 
+    displayItems();
+});
