04cc88c6422beb2dbabeef5107e36693a6d05c70
Dorian Bishop <dorianbishop427@gmail.com>
2019-11-18 13:43:58 -0500

    added table and connection query

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -21,8 +21,27 @@ connection.connect((err) => {   if (err) throw err;
 // Global Variables.
 var chosenItem = {};
 
-// Function to reset the chosenItem array.
+// Function to reset the chosen item array.
 
 var resetCart = function() {
     chosenItem = {};
 }
+
+// function to display all items for sale
+var displayItems = function() {
+    connection.query(`SELECT * FROM products`, (err, res) => {
+        var listTable = new Table({
+            head: ['item_id', 'product_name', 'price'],
+            colWidths: [10, 45, 12]
+        });
+
+        for (var i = 0; i < res.length; i++) {
+            listTable.push([res[i].item_id, res[i].product_name, `${res[i].price}`]);
+           
+        }
+        
+        console.log(`\n\n${listTable.toString()}\n\n`);
+        // ask user to enter ID of item they wish to purchase
+        askForID();
+    });
+};
